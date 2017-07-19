7a498f95d9b812e44378990e466251f8d3d441d8
danielpdoherty <danielpdoherty@gmail.com>
2017-07-19 15:37:59 -0500

    added some intial jsx for header and install node_modules for dev computer

--- todohompage/src/components/Header.js
+++ todohompage/src/components/Header.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
+	constructor(props){
+		super(props);
+
+		this.state = {
+			date: new Date().toLocaleDateString(),
+			time: new Date().toLocaleTimeString()
+		}
+	}
+
   render() {
     return (
       <div className="header">
+      	<header>
+			<div className="welcomeMessage">
+				<h1>Welcome</h1>
+				<h1{this.state.date}</h1>
+				<h1>{this.state.time}</h1>
+			</div>
+			<AddTask />
+		</header>
       </div>
     );
   }
