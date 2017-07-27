d5d7fe7bfb8f46000a19fdd4c4367f704058daf5
Dan <danielpdoherty@gmail.com>
2017-07-27 17:45:00 -0500

    adding bootstrap and css for header footer

--- todohompage/src/components/Footer.js
+++ todohompage/src/components/Footer.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import './footer.css';
 
 class Footer extends Component {
   render() {
     return (
       <div className="footer">
+      	<footer>
+			<section class="col-lg-3 col-sm-12">
+				recently completed
+			</section>
+			<h3 class="col-lg-3 col-sm-12">
+				Daniel Doherty 2017.
+			</h3>
+			<a href="#" class="col-lg-3 col-sm-12">
+				<img src="#" alt="Contact Dan" />
+			</a>
+		</footer>	
       </div>
     );
   }
