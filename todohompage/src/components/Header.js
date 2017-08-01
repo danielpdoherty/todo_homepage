02f02726ec9d73ee92a6a36a94eddead41142955
Dan <danielpdoherty@gmail.com>
2017-08-01 18:00:17 -0500

    functionality in header for auto-date change

--- todohompage/src/components/Header.js
+++ todohompage/src/components/Header.js
@@ -16,10 +16,15 @@ class Header extends Component {
       () => this.tick(),
       1000
     );
+    this.sunDial = setInterval(
+      () => this.sunDial(),
+      10800000
+    );
   }
 
   componentWillUnmount() {
     clearInterval(this.timerID);
+    clearInterval(this.sunDial);
   }
 
   tick() {
@@ -28,17 +33,25 @@ class Header extends Component {
     });
   }
 
+  sunDial(){
+    this.setState({
+      date: new Date().toLocaleDateString()
+    });
+  }
+
+  //10,800,000 ms
+
   render() {
     return (
       <div className="header">
       	<header>
-			<div className="welcomeMessage">
-				<h1>Welcome</h1>
-				<h1>{this.state.date}</h1>
-				<h1>{this.state.time}</h1>
-			</div>
-			<AddTask />
-		</header>
+    			<div className="welcomeMessage">
+    				<h1>Welcome</h1>
+    				<h1>{this.state.date}</h1>
+    				<h1>{this.state.time}</h1>
+    			</div>
+			    <AddTask />
+		    </header>
       </div>
     );
   }
